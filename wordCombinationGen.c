a210b99a9ce2241bfd8c322d057652250fdf69b0
Meshaal ALzahrani <mna3623@hotmail.com>
2023-11-22 01:35:31 +0300

    1

---
+++ wordCombinationGen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    return 0;
+}
+char *Gen(char[] data){
+    FILE *fh_output;
+    fh_output = fopen("wordCombination.txt", "w");
+}
