f7f9804f4a23881dea300e6fa1af4fdab3419c94
Meshaal ALzahrani <mna3623@hotmail.com>
2023-11-23 17:11:15 +0300

    1

--- wordCombFinder.c
+++ wordCombFinder.c
@@ -3,40 +3,51 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int isword(char* word,char* upperbound,char* lowerbound , FILE *thewordsfile);
+int isword(char *word, char *upperbound, char *lowerbound, FILE *thewordsfile);
 void generateWords(int number[], char word[], int index, FILE *thewordsfile);
 
-int main(){
+int main()
+{
 
     return 0;
 }
 
-int isword(char* word,char* upperbound,char* lowerbound , FILE *thewordsfile){
+int isword(char *word, char *upperbound, char *lowerbound, FILE *thewordsfile)
+{
     char line[8];
-    if(!(thewordsfile)){
+    if (!(thewordsfile))
+    {
         thewordsfile = ("sevenletterword.txt", "r");
-        if(!(thewordsfile)){
+        if (!(thewordsfile))
+        {
             printf("Error opening word alpha file \n");
             return 1;
         }
     }
-    do{
-        fscanf(thewordsfile, "%s" , line);
-    }while(strcmp(upperbound,line) > 0);
-    if(strcmp(word,line) == 0){
+    do
+    {
+        fscanf(thewordsfile, "%s", line);
+    } while (strcmp(upperbound, line) > 0);
+    if (strcmp(word, line) == 0)
+    {
         return 1;
     }
-    while(fscanf(thewordsfile, "%s" , line) != EOF){
-        if(strcmp(word,line) == 0){
+    while (fscanf(thewordsfile, "%s", line) != EOF)
+    {
+        if (strcmp(word, line) == 0)
+        {
             return 1;
         }
-        if(strcmp(lowerbound,line) < 0){
+        if (strcmp(lowerbound, line) < 0)
+        {
             return 0;
         }
     }
 }
-void generateWords(int number[], char word[], int index, FILE *file) {
-    if (index == 7) {
+void generateWords(int number[], char word[], int index, FILE *file)
+{
+    if (index == 7)
+    {
         fprintf(file, "%s\n", word);
         return;
     }
@@ -45,59 +56,135 @@ void generateWords(int number[], char word[], int index, FILE *file) {
     char letters[4];
     int count = 0;
 
-    switch (digit) {
-        case 2:
-            letters[0] = 'A';
-            letters[1] = 'B';
-            letters[2] = 'C';
-            count = 3;
-            break;
-        case 3:
-            letters[0] = 'D';
-            letters[1] = 'E';
-            letters[2] = 'F';
-            count = 3;
-            break;
-        case 4:
-            letters[0] = 'G';
-            letters[1] = 'H';
-            letters[2] = 'I';
-            count = 3;
-            break;
-        case 5:
-            letters[0] = 'J';
-            letters[1] = 'K';
-            letters[2] = 'L';
-            count = 3;
-            break;
-        case 6:
-            letters[0] = 'M';
-            letters[1] = 'N';
-            letters[2] = 'O';
-            count = 3;
-            break;
-        case 7:
-            letters[0] = 'P';
-            letters[1] = 'R';
-            letters[2] = 'S';
-            count = 3;
-            break;
-        case 8:
-            letters[0] = 'T';
-            letters[1] = 'U';
-            letters[2] = 'V';
-            count = 3;
-            break;
-        case 9:
-            letters[0] = 'W';
-            letters[1] = 'X';
-            letters[2] = 'Y';
-            count = 3;
-            break;
+    switch (digit)
+    {
+    case 2:
+        letters[0] = 'a';
+        letters[1] = 'b';
+        letters[2] = 'c';
+        count = 3;
+        break;
+    case 3:
+        letters[0] = 'd';
+        letters[1] = 'e';
+        letters[2] = 'f';
+        count = 3;
+        break;
+    case 4:
+        letters[0] = 'g';
+        letters[1] = 'h';
+        letters[2] = 'i';
+        count = 3;
+        break;
+    case 5:
+        letters[0] = 'j';
+        letters[1] = 'k';
+        letters[2] = 'l';
+        count = 3;
+        break;
+    case 6:
+        letters[0] = 'm';
+        letters[1] = 'n';
+        letters[2] = 'o';
+        count = 3;
+        break;
+    case 7:
+        letters[0] = 'p';
+        letters[1] = 'r';
+        letters[2] = 's';
+        count = 3;
+        break;
+    case 8:
+        letters[0] = 't';
+        letters[1] = 'u';
+        letters[2] = 'v';
+        count = 3;
+        break;
+    case 9:
+        letters[0] = 'w';
+        letters[1] = 'x';
+        letters[2] = 'y';
+        count = 3;
+        break;
     }
 
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++)
+    {
         word[index] = letters[i];
         generateWords(number, word, index + 1, file);
     }
+}
+void getlower(char lower[], int number[])
+{
+    int digit = 0;
+    int i = 0;
+    for (i = 0; i < 7; i++)
+        digit = number[i];
+    {
+        switch (digit)
+        {
+        case 2 lower[i] = 'a';
+            break;
+            case 3:
+            lower[i] = 'd';
+            break;
+        case 4:
+            lower[i] = 'g';
+            break;
+        case 5:
+            lower[i] = 'j';
+            break;
+        case 6:
+            lower[i] = 'm';
+            break;
+        case 7:
+            lower[i] = 'p';
+            break;
+        case 8:
+            lower[i] = 't';
+            break;
+        case 9:
+            lower[i] = 'w';
+        default:
+            break;
+        }
+    }
+    return;
+}
+void getupper(char upper[], int number[])
+{
+    int digit = 0;
+    int i = 0;
+    for (i = 0; i < 7; i++)
+        digit = number[i];
+    {
+        switch (digit)
+        {
+        case 2 upper[i] = 'c';
+            break;
+            case 3:
+            upper[i] = 'f';
+            break;
+        case 4:
+            upper[i] = 'i';
+            break;
+        case 5:
+            upper[i] = 'l';
+            break;
+        case 6:
+            upper[i] = 'o';
+            break;
+        case 7:
+            upper[i] = 's';
+            break;
+        case 8:
+            upper[i] = 'v';
+            break;
+        case 9:
+            upper[i] = 'y';
+        default:
+            break;
+        }
+    }
+    return;
 }
